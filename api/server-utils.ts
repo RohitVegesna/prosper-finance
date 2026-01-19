f84f898ce5205d05dad6bfeb3828637f8e966a6e
Rohit Vegesna (Flexera) <rohit.vegesna+flexera@flexera.com>
2026-01-19 15:14:31 +0100

    Fix DatabaseStorage constructor call

--- api/server-utils.ts
+++ api/server-utils.ts
@@ -1,11 +1,10 @@
 // Server utilities for Vercel deployment
 // This file consolidates server functionality for the serverless environment
 
-import { db } from "./db";
 import { DatabaseStorage } from "../server/storage";
 
 // Initialize storage
-export const storage = new DatabaseStorage(db);
+export const storage = new DatabaseStorage();
 
 // Re-export everything needed for the API
 export { registerSimpleAuthRoutes } from "../server/simple-auth";
