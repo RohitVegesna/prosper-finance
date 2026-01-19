ef4adcef1febc72d45012a1ae4be4429f004c5be
Rohit Vegesna (Flexera) <rohit.vegesna+flexera@flexera.com>
2026-01-19 15:12:42 +0100

    storage

--- api/server-utils.ts
+++ api/server-utils.ts
@@ -2,10 +2,10 @@
 // This file consolidates server functionality for the serverless environment
 
 import { db } from "./db";
-import { Storage } from "../server/storage";
+import { DatabaseStorage } from "../server/storage";
 
 // Initialize storage
-export const storage = new Storage(db);
+export const storage = new DatabaseStorage(db);
 
 // Re-export everything needed for the API
 export { registerSimpleAuthRoutes } from "../server/simple-auth";
